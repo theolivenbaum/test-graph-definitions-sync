d61de415843078d071367128ee791bb1736f9405
admin admin <admin@localhost>
2025-04-03 15:30:23 +0000

    Updated definitions at 2025-04-03 15:30:23Z (PUT /api/scheduling/new)

---
+++ definitions/scheduled-tasks/custom-code-scheduled-task2-gFNYyeKvGatEdG9W1zToab.cs
@@ -0,0 +1,5 @@
+﻿[task: Curiosity.Tasks.Name("Custom Code Scheduled Task2")]
+[task: Curiosity.Tasks.UID("gFNYyeKvGatEdG9W1zToab")]
+[task: Curiosity.Tasks.Schedule("0 1 * * *")]
+
+Logger.LogInformation("Task run at: {0:u}", DateTimeOffset.UtcNow);
