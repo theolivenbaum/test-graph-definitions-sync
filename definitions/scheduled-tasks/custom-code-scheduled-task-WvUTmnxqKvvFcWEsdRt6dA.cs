19cc9be3408686600762e77b250e5064ba27c272
Curiosity Workspace <hello@curiosity.ai>
2025-04-03 09:05:44 +0000

    Updated definitions at 2025-04-03 09:05:44Z (POST /api/scheduling/update/schedule)

--- definitions/scheduled-tasks/custom-code-scheduled-task-WvUTmnxqKvvFcWEsdRt6dA.cs
+++ definitions/scheduled-tasks/custom-code-scheduled-task-WvUTmnxqKvvFcWEsdRt6dA.cs
@@ -1,5 +1,5 @@
 ﻿[task: Curiosity.Tasks.Name("Custom Code Scheduled Task")]
 [task: Curiosity.Tasks.UID("WvUTmnxqKvvFcWEsdRt6dA")]
-[task: Curiosity.Tasks.Schedule("0 1 * * *")]
+[task: Curiosity.Tasks.Schedule("0 2 * * *")]
 
 Logger.LogInformation("Task run at: {0:u}", DateTimeOffset.UtcNow);
