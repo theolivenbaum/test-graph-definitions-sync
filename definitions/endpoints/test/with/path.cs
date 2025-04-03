9a729a01f29724dadf0d96cc960d3ea7f987b027
admin admin <admin@localhost>
2025-04-03 08:52:09 +0000

    Updated definitions at 2025-04-03 08:52:09Z (POST /api/endpoints/create)

---
+++ definitions/endpoints/test/with/path.cs
@@ -0,0 +1,7 @@
+﻿[endpoint: Curiosity.Endpoints.Path("test/with/path")]
+[endpoint: Curiosity.Endpoints.ForAdmins]
+
+Console.WriteLine("here");
+Console.WriteLine("here");
+Console.WriteLine("here");
+Console.WriteLine("here");
