6bbf98950ea7f5d8e60c24aa14a4acb17242b4a4
Curiosity Workspace <hello@curiosity.ai>
2025-04-03 09:07:35 +0000

    Updated definitions at 2025-04-03 09:07:35Z (DELETE /api/endpoints/delete)

--- definitions/endpoints/test/with/path.cs
+++
@@ -1,7 +0,0 @@
-﻿[endpoint: Curiosity.Endpoints.Path("test/with/path")]
-[endpoint: Curiosity.Endpoints.ForAdmins]
-
-Console.WriteLine("here");
-Console.WriteLine("here");
-Console.WriteLine("here");
-Console.WriteLine("here");
